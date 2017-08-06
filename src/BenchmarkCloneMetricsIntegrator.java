2c22361223476eedc755fce502830648fe585111
Farima Farahani <Farima Farahani>
2017-08-06 12:37:04 -0700

    work based on splitted files

--- src/BenchmarkCloneMetricsIntegrator.java
+++ src/BenchmarkCloneMetricsIntegrator.java
@@ -20,9 +20,11 @@ public class BenchmarkCloneMetricsIntegrator {
 
     public BenchmarkCloneMetricsIntegrator(){
         try {
+            System.out.println("enter source file name:");
             Scanner sc=new Scanner(System.in);
             sourceFile=sc.next();
             sourceFile="./new_benchmark/"+sourceFile;
+            outputPath="./output/benchmark_integrated_"+sourceFile+".txt";
  //           File folder = new File(inputMetricsPath);
    //         File[] files = folder.listFiles();
 //            for (int i = 0; i < files.length; i++) {
